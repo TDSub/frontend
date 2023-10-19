2b6b88a2204ca38cece1e1d8d132d8d322ea4085
Houman <houmansnt@gmail.com>
2023-10-19 22:17:37 +0330

    feat: added ton connect

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,21 @@
 import { Grid, Typography } from '@mui/material'
+import { TonConnectButton } from '@tonconnect/ui-react'
 
 const Navbar = () => {
   return (
     <Grid
       container
       alignItems={'center'}
+      justifyContent={'space-between'}
       sx={theme => ({ backgroundColor: theme.palette.background.paper, paddingX: 2 })}
     >
       <Grid item>
         <Typography fontWeight={'bold'} fontSize={20}>
           Decentralized Subscription Protocol
         </Typography>
+      </Grid>
+      <Grid item>
+        <TonConnectButton />
       </Grid>
     </Grid>
   )
--- src/layouts/CommonLayout.tsx
+++ src/layouts/CommonLayout.tsx
@@ -1,16 +1,21 @@
 import { ThemeProvider } from '@mui/material'
 import CssBaseline from '@mui/material/CssBaseline'
+import { TonConnectUIProvider } from '@tonconnect/ui-react'
 import { FC, PropsWithChildren } from 'react'
 import { BrowserRouter } from 'react-router-dom'
 
 import theme from '../theme.tsx'
 
 const CommonLayout: FC<PropsWithChildren> = ({ children }) => {
+  // this manifest is used temporarily for development purposes
+  const manifestUrl =
+    'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json'
+
   return (
     <BrowserRouter>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        {children}
+        <TonConnectUIProvider manifestUrl={manifestUrl}>{children}</TonConnectUIProvider>
       </ThemeProvider>
     </BrowserRouter>
   )
